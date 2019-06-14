85da152fef893c3e6a90905049a332495b44c50d
Phil Vachon <phil@vachon.nyc>
2019-06-14 07:08:33 -0400

    Fix white space, yay Visual Studio

--- hantek_tester/hantek_tester.cpp
+++ hantek_tester/hantek_tester.cpp
@@ -12,60 +12,60 @@
 
 short find_devs()
 {
-	short dev_info[MAX_USB_DEV_NUMBER];
-	WORD nr_devs = dsoHTSearchDevice(dev_info),
-		fpga_version = 0,
-		hard_version = 0;
+    short dev_info[MAX_USB_DEV_NUMBER];
+    WORD nr_devs = dsoHTSearchDevice(dev_info),
+        fpga_version = 0,
+        hard_version = 0;
 
-	std::cout << "Found " << nr_devs << " devices" << std::endl;
+    std::cout << "Found " << nr_devs << " devices" << std::endl;
 
-	for (size_t i = 0; i < nr_devs; i++) {
-		std::cout << "    device: " << dev_info[i] << std::endl;
-	}
+    for (size_t i = 0; i < nr_devs; i++) {
+        std::cout << "    device: " << dev_info[i] << std::endl;
+    }
 
-	short dev_idx = 0; // For now, assume we're just opening the first device we found.
+    short dev_idx = 0; // For now, assume we're just opening the first device we found.
 
-	// This is broken?
-	//if (FALSE == dsoSetUSBBus(dev_idx)) {
-	//	std::cout << "Failed to dsoSetUSBBus()" << std::endl;
-	//	nr_devs = 0;
-	//	goto done;
-	//}
+    // This is broken?
+    //if (FALSE == dsoSetUSBBus(dev_idx)) {
+    //    std::cout << "Failed to dsoSetUSBBus()" << std::endl;
+    //    nr_devs = 0;
+    //    goto done;
+    //}
 
-	if (1 == dsoHTDeviceConnect(dev_idx)) {
-		std::cout << "Device is connected!" << std::endl;
-	}
-	else {
-		std::cerr << "Device connection failure, aborting." << std::endl;
-		nr_devs = 0;
-		goto done;
-	}
+    if (1 == dsoHTDeviceConnect(dev_idx)) {
+        std::cout << "Device is connected!" << std::endl;
+    }
+    else {
+        std::cerr << "Device connection failure, aborting." << std::endl;
+        nr_devs = 0;
+        goto done;
+    }
 
-	if (0 == dsoInitHard(dev_idx)) {
-		std::cout << "Error while initializing device " << dev_idx << std::endl;
-		nr_devs = 0;
-		goto done;
-	}
+    if (0 == dsoInitHard(dev_idx)) {
+        std::cout << "Error while initializing device " << dev_idx << std::endl;
+        nr_devs = 0;
+        goto done;
+    }
 
-	fpga_version = dsoGetFPGAVersion(dev_idx);
-	hard_version = dsoGetHardVersion(dev_idx);
-	std::cout << "FPGA version: 0x" << std::ios::hex <<  std::setw(4) << std::setfill('0') << " Hardware version: 0x" << hard_version << std::endl;
+    fpga_version = dsoGetFPGAVersion(dev_idx);
+    hard_version = dsoGetHardVersion(dev_idx);
+    std::cout << "FPGA version: 0x" << std::ios::hex <<  std::setw(4) << std::setfill('0') << " Hardware version: 0x" << hard_version << std::endl;
 
 
 
 
 
 done:
-	return nr_devs;
+    return nr_devs;
 }
 
 int main()
 {
     std::cout<< "The Hantek Tester" << std::endl;
 
-	find_devs();
+    find_devs();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 
